120736831d4862c436c46eee6c193c9cc5118f69
DarioOjeda <darioojeda_1995@hotmail.com>
2021-08-11 11:30:03 +0200

    componente de graficas de barra personalizable con inputs

--- src/app/graficas/components/grafica-barra/grafica-barra.component.ts
+++ src/app/graficas/components/grafica-barra/grafica-barra.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ChartDataSets, ChartOptions, ChartType } from 'chart.js';
+import { Label } from 'ng2-charts';
 
 @Component({
   selector: 'app-grafica-barra',
@@ -7,10 +9,35 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class GraficaBarraComponent implements OnInit {
+  
+  @Input() horizontal: boolean = false;
+  
+  public barChartOptions: ChartOptions = {
+    responsive: true,
+  };
+
+
+  @Input() barChartLabels: Label[] = [
+    //'2015', '2016', '2017', '2018', '2019', '2020', '2021'
+  ];
+
+  public barChartType: ChartType = 'bar'; 
+  public barChartLegend = true;
+
+  @Input() barChartData: ChartDataSets[] = [
+    // { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A', backgroundColor: '#DBBC81', hoverBackgroundColor: 'red'},
+    // { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B', backgroundColor: '#FA725A', hoverBackgroundColor: 'red'},
+    // { data: [18, 38, 100, 0, 80, 45, 23], label: 'Series C', backgroundColor: '#38D961', hoverBackgroundColor: 'red'}
+  ];
 
   constructor() { }
 
   ngOnInit(): void {
+    if( this.horizontal ) {
+      this.barChartType = "horizontalBar";
+    }
   }
+
+
 
 }
--- src/app/graficas/pages/barras/barras.component.ts
+++ src/app/graficas/pages/barras/barras.component.ts
@@ -13,21 +13,15 @@ export class BarrasComponent implements OnInit {
   public barChartOptions: ChartOptions = {
     responsive: true,
     // We use these empty structures as placeholders for dynamic theming.
-    scales: { xAxes: [{}], yAxes: [{}] },
-    plugins: {
-      datalabels: {
-        anchor: 'end',
-        align: 'end',
-      }
-    }
   };
-  public barChartLabels: Label[] = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
-  public barChartType: ChartType = 'bar';
+  public barChartLabels: Label[] = ['2015', '2016', '2017', '2018', '2019', '2020', '2021'];
+  public barChartType: ChartType = 'bar'; 
   public barChartLegend = true;
 
   public barChartData: ChartDataSets[] = [
-    { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A' },
-    { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B' }
+    { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A', backgroundColor: '#DBBC81', hoverBackgroundColor: 'red'},
+    { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B', backgroundColor: '#FA725A', hoverBackgroundColor: 'red'},
+    { data: [18, 38, 100, 0, 80, 45, 23], label: 'Series C', backgroundColor: '#38D961', hoverBackgroundColor: 'red'}
   ];
 
   constructor() { }
@@ -36,15 +30,39 @@ export class BarrasComponent implements OnInit {
   }
 
   public randomize(): void {
-    // Only Change 3 values
+
     this.barChartData[0].data = [
-      Math.round(Math.random() * 100),
-      59,
-      80,
-      (Math.random() * 100),
-      56,
-      (Math.random() * 100),
-      40 ];
+      Math.round( Math.random() * 100 ),
+      Math.round( Math.random() * 100 ),
+      Math.round( Math.random() * 100 ),
+      Math.round( Math.random() * 100 ),
+      Math.round( Math.random() * 100 ),
+      Math.round( Math.random() * 100 ),
+      Math.round( Math.random() * 100 ),
+      Math.round( Math.random() * 100 )
+    ];
+
+    this.barChartData[1].data = [
+      Math.round( Math.random() * 100 ),
+      Math.round( Math.random() * 100 ),
+      Math.round( Math.random() * 100 ),
+      Math.round( Math.random() * 100 ),
+      Math.round( Math.random() * 100 ),
+      Math.round( Math.random() * 100 ),
+      Math.round( Math.random() * 100 ),
+      Math.round( Math.random() * 100 )
+    ];
+
+    this.barChartData[2].data = [
+      Math.round( Math.random() * 100 ),
+      Math.round( Math.random() * 100 ),
+      Math.round( Math.random() * 100 ),
+      Math.round( Math.random() * 100 ),
+      Math.round( Math.random() * 100 ),
+      Math.round( Math.random() * 100 ),
+      Math.round( Math.random() * 100 ),
+      Math.round( Math.random() * 100 )
+    ];
   }
 
 }
